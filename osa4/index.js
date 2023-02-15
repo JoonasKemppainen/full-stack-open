51240deb2d7c60b7f29c3c65f215f33c07e9300b
Joonas Kemppainen <karmakebu@gmail.com>
2023-02-15 14:50:29 +0200

    first commit

---
+++ osa4/index.js
@@ -0,0 +1,7 @@
+const {info, error} = require('./utils/logger')
+const {PORT} = require('./utils/config')
+const app = require('./app')
+
+app.listen(PORT, () => {
+  info(`Server running on port ${PORT}`)
+})
---
+++ osa4/models/blog.js
@@ -0,0 +1,22 @@
+const {MONGODB_URI} = require('../utils/config')
+const mongoose = require('mongoose')
+mongoose.set("strictQuery", false)
+mongoose.connect(MONGODB_URI)
+
+const blogSchema = mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
+})
+
+// blogSchema.set('toJSON', {
+//     transform: (document, returnedObject) => {
+//       returnedObject.id = returnedObject._id.toString()
+//       delete returnedObject._id
+//       delete returnedObject.__v
+//     }
+//   })
+
+module.exports = mongoose.model('Blog', blogSchema)
+
