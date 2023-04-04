1ced3a112d93a790a0e96fc35ff48f1c587165b8
Joonas Kemppainen <karmakebu@gmail.com>
2023-04-04 10:46:17 +0300

    10.8

--- osa10/rate-repository-app/src/components/SignIn.jsx
+++ osa10/rate-repository-app/src/components/SignIn.jsx
@@ -1,7 +1,65 @@
 import Text from './Text';
+import FormikTextInput from './FormikTextInput';
+import { Pressable, View, StyleSheet } from 'react-native';
+import { Formik } from 'formik';
+
+const initialValues = {
+  username: "",
+  password: "",
+};
+
+const styles = StyleSheet.create({
+    input: {
+      height: 50,
+      margin: 12,
+      borderWidth: 1,
+      padding: 10,
+      borderRadius: 3,
+    },
+    signin: {
+        borderRadius: 3,
+        margin: 12,
+        color: "#FFFFFF",
+        height: 55,
+        backgroundColor: "#0165D4",
+        textAlign: "center",
+        textAlignVertical: "center"
+    }
+  });
+
+const SignInForm = ({ onSubmit }) => {
+  return (
+    <View>
+      <FormikTextInput 
+        name="username" 
+        placeholder="Username" 
+        style={styles.input} />
+      <FormikTextInput 
+        name="password" 
+        placeholder="Password" 
+        style={styles.input}
+        secureTextEntry={true} />
+      <Pressable onPress={onSubmit}>
+        <Text 
+          color="white" 
+          fontsize="subheading" 
+          fontWeight="bold" 
+          style={styles.signin}>Sign in</Text>
+      </Pressable>
+    </View>
+  )
+};
 
 const SignIn = () => {
-  return <Text>The sign-in view</Text>;
-};
+  const onSubmit = values => {
+    console.log(values)
+  }
+
+  return (
+    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+      {({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
+    </Formik>
+  )
+}
 
 export default SignIn;
